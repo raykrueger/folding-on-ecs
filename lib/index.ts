eb5c7402d4ba301dbeff86916f79fc6dfca1fbe2
Ray Krueger <raykrueger@gmail.com>
2020-03-26 17:13:17 +0000

    Package as a cdk library

--- lib/index.ts
+++ lib/index.ts
@@ -1,32 +1,109 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import {Construct, Duration} from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as logs from '@aws-cdk/aws-logs';
 
-export interface FoldingOnEcsClusterProps {
+export interface FoldingAtHomeProps {
   /**
-   * The visibility timeout to be configured on the SQS Queue, in seconds.
-   *
-   * @default Duration.seconds(300)
+   * VPC to deploy to, this will override the maxAzs property.
+   * @default None
    */
-  visibilityTimeout?: cdk.Duration;
+  vpc?: ec2.IVpc;
+
+  /**
+   * How many host instances should we launch?
+   * @default 3
+   */
+  clusterSize?: number;
+
+  /**
+   * The maximum hourly price (in USD) to be paid for any Spot Instance launched to fulfill the request.
+   * @default 0.50
+   */
+  spotPrice?: string;
+
+  /**
+   * The folding home container to run
+   * @default raykrueger/folding-at-home
+   */
+  image?: ecs.ContainerImage;
+
+  /**
+   * The cloudwatch log stream prefix
+   * @default 'folding'
+   */
+  streamPrefix?: string;
+
+  /**
+   * Should container insights be enabled?
+   * @default false
+   */
+  containerInsights?: boolean;
 }
 
-export class FoldingOnEcsCluster extends cdk.Construct {
-  /** @returns the ARN of the SQS queue */
-  public readonly queueArn: string;
+/**
+ * Builds an 3 node ECS Cluster of g4dn.xlarge Spot instances to run the Folding
+ * at Home container on, and the VPC to support. The default VPC can be
+ * overridden by passing a VPC instance into the FoldingAtHomeProps.
+ */
+export class FoldingAtHome extends Construct {
 
-  constructor(scope: cdk.Construct, id: string, props: FoldingOnEcsClusterProps = {}) {
+  // Declare our defaults
+  public static readonly DEFAULT_MAX_AZS = 3;
+  public static readonly DEFAULT_CLUSTER_SIZE = 3;
+  public static readonly DEFAULT_SPOT_PRICE = '0.52';
+  public static readonly DEFAULT_IMAGE = 'raykrueger/folding-at-home';
+  public static readonly DEFAULT_STREAM_PREFIX = 'folding';
+
+  public readonly ecsCluster: ecs.Cluster;
+  public readonly taskDef: ecs.TaskDefinition;
+  public readonly containerDef: ecs.ContainerDefinition;
+  public readonly service: ecs.Ec2Service;
+
+  constructor(scope: Construct, id: string, props: FoldingAtHomeProps={}) {
     super(scope, id);
 
-    const queue = new sqs.Queue(this, 'FoldingOnEcsClusterQueue', {
-      visibilityTimeout: props.visibilityTimeout || cdk.Duration.seconds(300)
+    //File in the defaults
+    props.streamPrefix = props.streamPrefix || FoldingAtHome.DEFAULT_STREAM_PREFIX;
+    props.clusterSize = props.clusterSize || FoldingAtHome.DEFAULT_CLUSTER_SIZE;
+    props.spotPrice = props.spotPrice || FoldingAtHome.DEFAULT_SPOT_PRICE;
+    props.image = props.image || ecs.ContainerImage.fromRegistry(FoldingAtHome.DEFAULT_IMAGE);
+
+    //The real stuff. Define our ECS GPU Cluster
+    this.ecsCluster = new ecs.Cluster(this, 'FoldingCluster', {
+      vpc: props.vpc,
+      capacity: {
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.G4DN, ec2.InstanceSize.XLARGE),
+        spotPrice: props.spotPrice,
+        taskDrainTime: Duration.seconds(0),
+        machineImage: ecs.EcsOptimizedImage.amazonLinux2(ecs.AmiHardwareType.GPU),
+        minCapacity: props.clusterSize,
+      },
+      containerInsights: props.containerInsights
     });
 
-    const topic = new sns.Topic(this, 'FoldingOnEcsClusterTopic');
+    //kind of obvious
+    this.taskDef = new ecs.TaskDefinition(this, 'FoldingAtHomeTask', {
+      compatibility: ecs.Compatibility.EC2
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    //The container def, I only allow half of the ram
+    this.containerDef = new ecs.ContainerDefinition(this, 'main', {
+      image: props.image,
+      taskDefinition: this.taskDef,
+      memoryLimitMiB: 8192,
+      gpuCount: 1,
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: props.streamPrefix, logRetention: logs.RetentionDays.ONE_WEEK
+      })
+    });
 
-    this.queueArn = queue.queueArn;
+    //Define our service as a daemon so we run the container on every node
+    this.service = new ecs.Ec2Service(this, 'FoldingService', {
+      cluster: this.ecsCluster,
+      taskDefinition: this.taskDef,
+      daemon: true
+    });
+
   }
 }
--- test/folding-on-ecs-cluster.test.ts
+++ test/folding-on-ecs-cluster.test.ts
@@ -1,21 +1,68 @@
-import { expect as expectCDK, haveResource, SynthUtils } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource, countResourcesLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import FoldingOnEcsCluster = require('../lib/index');
+import * as ecs from '@aws-cdk/aws-ecs';
+import {FoldingAtHome} from '../lib/index'
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, "TestStack");
-    // WHEN
-    new FoldingOnEcsCluster.FoldingOnEcsCluster(stack, 'MyTestConstruct');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue"));
+//added so I could verify it is safe to remove my default vpc, and let the ecs.Cluster do it
+test('Creates Default VPC', () => {
+  const app = new cdk.App();
+  class TestStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+      const cluster = new FoldingAtHome(this, 'FoldingAtHome');
+    }
+  }
+
+  const stack = new TestStack(app, 'TestStack');
+
+  expectCDK(stack).to(haveResource("AWS::EC2::VPC"));
 });
 
-test('SNS Topic Created', () => {
+test('Deploys default image', () => {
   const app = new cdk.App();
-  const stack = new cdk.Stack(app, "TestStack");
-  // WHEN
-  new FoldingOnEcsCluster.FoldingOnEcsCluster(stack, 'MyTestConstruct');
-  // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+
+  class TestStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+      const cluster = new FoldingAtHome(this, 'FoldingAtHome');
+    }
+  }
+
+  const stack = new TestStack(app, 'TestStack');
+
+  //cdk assert bug? haveResource would not match this, but countResourcesLike works
+  expectCDK(stack).to(countResourcesLike('AWS::ECS::TaskDefinition', 1, {
+    ContainerDefinitions: [
+      {
+        Essential: true,
+        Image: 'raykrueger/folding-at-home'
+      }
+    ]
+  }));
+});
+
+test('Deploys custom image', () => {
+  const app = new cdk.App();
+
+  class TestStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+
+      const cluster = new FoldingAtHome(this, 'FoldingAtHome', {
+        image: ecs.RepositoryImage.fromRegistry('example/example')
+      });
+    }
+  }
+
+  const stack = new TestStack(app, 'TestStack');
+
+  //cdk assert bug? haveResource would not match this, but countResourcesLike works
+  expectCDK(stack).to(countResourcesLike('AWS::ECS::TaskDefinition', 1, {
+    ContainerDefinitions: [
+      {
+        Essential: true,
+        Image: 'example/example'
+      }
+    ]
+  }));
 });
