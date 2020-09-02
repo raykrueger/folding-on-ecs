7ee5345611090a43913579b99d46662cf8d3e5a8
Ray Krueger <raykrueger@gmail.com>
2020-09-02 08:27:12 -0500

    Simplify testing, add a snapshot

--- test/folding-on-ecs-cluster.test.ts
+++ test/folding-on-ecs-cluster.test.ts
@@ -1,34 +1,19 @@
-import { expect as expectCDK, matchTemplate, MatchStyle, haveResource, countResourcesLike } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResource, countResourcesLike, SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as ecs from '@aws-cdk/aws-ecs';
 import {FoldingAtHome} from '../lib/index'
+import { Stack } from '@aws-cdk/core';
 
 //added so I could verify it is safe to remove my default vpc, and let the ecs.Cluster do it
 test('Creates Default VPC', () => {
-  const app = new cdk.App();
-  class TestStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
-      const cluster = new FoldingAtHome(this, 'FoldingAtHome');
-    }
-  }
-
-  const stack = new TestStack(app, 'TestStack');
-
+  const stack = new Stack();
+  new FoldingAtHome(stack, 'FoldingAtHome');
   expectCDK(stack).to(haveResource("AWS::EC2::VPC"));
 });
 
 test('Deploys default image', () => {
-  const app = new cdk.App();
-
-  class TestStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
-      const cluster = new FoldingAtHome(this, 'FoldingAtHome');
-    }
-  }
-
-  const stack = new TestStack(app, 'TestStack');
+  const stack = new Stack();
+  new FoldingAtHome(stack, 'FoldingAtHome');
 
   //cdk assert bug? haveResource would not match this, but countResourcesLike works
   expectCDK(stack).to(countResourcesLike('AWS::ECS::TaskDefinition', 1, {
@@ -42,19 +27,10 @@ test('Deploys default image', () => {
 });
 
 test('Deploys custom image', () => {
-  const app = new cdk.App();
-
-  class TestStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
-
-      const cluster = new FoldingAtHome(this, 'FoldingAtHome', {
-        image: ecs.RepositoryImage.fromRegistry('example/example')
-      });
-    }
-  }
-
-  const stack = new TestStack(app, 'TestStack');
+  const stack = new Stack();
+  new FoldingAtHome(stack, 'FoldingAtHome', {
+    image: ecs.RepositoryImage.fromRegistry('example/example')
+  });
 
   //cdk assert bug? haveResource would not match this, but countResourcesLike works
   expectCDK(stack).to(countResourcesLike('AWS::ECS::TaskDefinition', 1, {
@@ -65,4 +41,11 @@ test('Deploys custom image', () => {
       }
     ]
   }));
+});
+
+test('Snapshot', () => {
+  const stack = new Stack();
+  new FoldingAtHome(stack, 'FoldingAtHome');
+
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
